418bf639fd81974e605f360259585e23abfe1be7
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2020-05-09 20:20:44 -0400

    don't rerender guess lines unnecessarily

--- ferrogallic_web/src/component/guess_area.rs
+++ ferrogallic_web/src/component/guess_area.rs
@@ -45,18 +45,66 @@ impl Component for GuessArea {
     }
 
     fn view(&self) -> Html {
-        let format_user = |user_id| {
-            self.props
-                .players
-                .get(&user_id)
-                .map(|p| &*p.nick)
-                .unwrap_or("<unknown>")
-        };
         let guesses = self
             .props
             .guesses
             .iter()
-            .map(|guess| match guess.as_ref() {
+            .map(|guess| html! { <guess::GuessLine players=self.props.players.clone() guess=guess.clone()/> })
+            .collect::<Html>();
+
+        html! {
+            <ul ref=self.area_ref.clone() class="tree-view" style="height: 100%; overflow-y: scroll">{guesses}</ul>
+        }
+    }
+}
+
+mod guess {
+    use super::*;
+
+    pub enum Msg {}
+
+    #[derive(Clone, Properties)]
+    pub struct Props {
+        pub players: Arc<BTreeMap<UserId, Player>>,
+        pub guess: Arc<Guess>,
+    }
+
+    impl PartialEq for Props {
+        fn eq(&self, Self { players, guess }: &Self) -> bool {
+            Arc::ptr_eq(&self.players, players) && Arc::ptr_eq(&self.guess, guess)
+        }
+    }
+
+    pub struct GuessLine {
+        props: Props,
+    }
+
+    impl Component for GuessLine {
+        type Message = Msg;
+        type Properties = Props;
+
+        fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+            Self { props }
+        }
+
+        fn update(&mut self, msg: Self::Message) -> ShouldRender {
+            match msg {}
+        }
+
+        fn change(&mut self, props: Self::Properties) -> ShouldRender {
+            self.props.neq_assign(props)
+        }
+
+        fn view(&self) -> Html {
+            let format_user = |user_id| {
+                self.props
+                    .players
+                    .get(&user_id)
+                    .map(|p| &*p.nick)
+                    .unwrap_or("<unknown>")
+            };
+
+            match self.props.guess.as_ref() {
                 Guess::System(system) => html! {
                     <li>{"🖥️ "}{system}</li>
                 },
@@ -78,10 +126,7 @@ impl Component for GuessArea {
                 Guess::TimeExpired => html! {
                     <li>{"⏰ Time's up!"}</li>
                 },
-            })
-            .collect::<Html>();
-        html! {
-            <ul ref=self.area_ref.clone() class="tree-view" style="height: 100%; overflow-y: scroll">{guesses}</ul>
+            }
         }
     }
 }
