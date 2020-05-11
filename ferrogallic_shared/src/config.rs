168cb9b9798e22820668e1bc81f342e146d1e151
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2020-05-10 22:59:57 -0400

    increase broadcast buffer size to 256
    We can easily transmit 100 canvas events per second, so keep this buffer big to avoid dropping clients.
    Also, this is still not _that_ big, a 256 element broadcast queue is probably only 32k.
    Doubling that for the sum of inbound queues = 64k * 16 games = 1MB, which is a lot of games per MB.
    We'll run out of CPU before memory at that rate.

--- ferrogallic_shared/src/config.rs
+++ ferrogallic_shared/src/config.rs
@@ -2,7 +2,7 @@ pub const MAX_REQUEST_BYTES: u64 = 4 * 1024;
 pub const MAX_WS_MESSAGE_BYTES: usize = 4 * 1024;
 
 pub const WS_RX_BUFFER_SHARED: usize = 64;
-pub const WS_TX_BUFFER_BROADCAST: usize = 64;
+pub const WS_TX_BUFFER_BROADCAST: usize = 256;
 
 pub const CANVAS_WIDTH: usize = 800;
 pub const CANVAS_HEIGHT: usize = 600;
